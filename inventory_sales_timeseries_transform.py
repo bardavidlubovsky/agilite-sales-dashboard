793da500a34835e90ff2b86b77d4998bdf4497b6
Bar-David Lubovsky <barlubo@icloud.com>
2025-06-18 21:36:44 +0300

    Update inventory_sales_timeseries_transform.py

--- inventory_sales_timeseries_transform.py
+++ inventory_sales_timeseries_transform.py
@@ -13,12 +13,10 @@ df = df.groupby(['Product_ID', 'Variant_Title', 'Snapshot_Timestamp'], as_index=
 
 df = df.sort_values(['Product_ID', 'Variant_Title', 'Snapshot_Timestamp'])
 df['Product_Variant_ID'] = df['Product_ID'].astype(str) + '_' + df['Variant_Title']
-
 df = df.groupby(['Product_Variant_ID', df['Snapshot_Timestamp'].dt.date], as_index=False).first()
 
 df['qty_change'] = df.groupby('Product_Variant_ID')['Variant_Qty'].diff().fillna(0)
 df['qty_sold'] = df['qty_change'].apply(lambda x: -x if x < 0 else 0)
-
 df['Sale_Date'] = pd.to_datetime(df['Snapshot_Timestamp']).dt.date
 df['Weekday'] = pd.to_datetime(df['Snapshot_Timestamp']).dt.day_name()
 df['Month'] = pd.to_datetime(df['Snapshot_Timestamp']).dt.month_name()
@@ -30,8 +28,14 @@ pivot_by_date = df.pivot_table(
     aggfunc='sum'
 ).fillna(0).reset_index()
 
-weekday_totals = df.groupby(['Product_Variant_ID', 'Weekday'])['qty_sold'].sum().unstack(fill_value=0).reset_index()
-month_totals = df.groupby(['Product_Variant_ID', 'Month'])['qty_sold'].sum().unstack(fill_value=0).reset_index()
+weekdays_order = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+weekday_totals = df.groupby(['Product_Variant_ID', 'Weekday'])['qty_sold'].sum().unstack().reindex(columns=weekdays_order, fill_value=0).reset_index()
+
+months_order = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+]
+month_totals = df.groupby(['Product_Variant_ID', 'Month'])['qty_sold'].sum().unstack().reindex(columns=months_order, fill_value=0).reset_index()
 
 final_df = pd.merge(pivot_by_date, weekday_totals, on='Product_Variant_ID', how='left')
 final_df = pd.merge(final_df, month_totals, on='Product_Variant_ID', how='left')
