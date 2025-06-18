368986db50187d1f4d45584919d07fc838aa42a6
Bar-David Lubovsky <barlubo@icloud.com>
2025-06-18 15:58:02 +0300

    Update generate_inventory_changes.py

--- generate_inventory_changes.py
+++ generate_inventory_changes.py
@@ -2,12 +2,10 @@ import pandas as pd
 
 df = pd.read_csv("data/historical_products.csv")
 df['Snapshot_Timestamp'] = pd.to_datetime(df['Snapshot_Timestamp'])
-
+df = df.drop_duplicates()
 df['row_number'] = df.groupby(['Product_ID', 'Variant_Title']).cumcount()
 df['product_variant_id'] = df['Product_ID'].astype(str) + '_' + df['Variant_Title'] + '_' + df['row_number'].astype(str)
-
 df = df.sort_values(['product_variant_id', 'Snapshot_Timestamp'])
-
 df['qty_change'] = df.groupby('product_variant_id')['Variant_Qty'].diff()
 
 added_df = df.copy()
@@ -21,7 +19,6 @@ added_total = (
 )
 
 df['timestamp_str'] = df['Snapshot_Timestamp'].dt.strftime('%Y-%m-%d %H:%M:%S')
-
 df = df.drop_duplicates(subset=['product_variant_id', 'timestamp_str'], keep='last')
 
 pivot_qty_change = df.pivot(index='product_variant_id', columns='timestamp_str', values='qty_change')
@@ -35,15 +32,12 @@ latest_rows = (
 )
 
 final_df = pivot_qty_change.merge(latest_rows, left_index=True, right_index=True)
-
 final_df.reset_index(inplace=True)
 final_df.insert(0, 'Product_Variant_ID', final_df['product_variant_id'])
 
 qty_cols = [col for col in final_df.columns if col[:4].isdigit()]
-
 final_df['Total_Sales'] = final_df[qty_cols].apply(lambda row: round(-row[row < 0].sum(), 2), axis=1)
 final_df['Total_Sales'] = final_df['Total_Sales'].replace(-0.0, 0.0)
-
 final_df['Total_Added'] = final_df['product_variant_id'].map(added_total).fillna(0.0)
 
 final_df.to_csv("data/inventory_changes_output.csv", index=False)
